e68051aa9c5b0c62e6495add2d401ea7ed50e248
ZOMini <zarikacr@gmail.com>
2023-04-14 15:33:50 +0300

    v3.0

--- billing_admin/billing/models.py
+++ billing_admin/billing/models.py
@@ -7,8 +7,8 @@ class UserStatus(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4,  editable=False)
     expires_at = models.DateTimeField(auto_now_add=False)
-    actual = models.BooleanField
-    expires_status = models.BooleanField
+    actual = models.BooleanField()
+    expires_status = models.BooleanField()
     # payments = models.ForeignKey('PaymentPG', on_delete=models.CASCADE)
     class Meta:
         db_table = "public\".\"userstatus"
@@ -18,9 +18,9 @@ class UserStatus(models.Model):
 class Tariff(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    days = models.DurationField(editable=False)
+    days = models.DurationField()
     price = models.FloatField(blank=False)
-    description = models.CharField('description', max_length=127, editable=False)
+    description = models.CharField('description', max_length=127)
 
     class Meta:
         db_table = "public\".\"tariff"
@@ -34,10 +34,10 @@ class PaymentPG(models.Model):
         refund = "refund"
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    created_at = models.DateTimeField(auto_now_add=False, editable=False)
-    payment = models.CharField('payment', max_length=127, editable=False)
-    income = models.FloatField(blank=False, editable=False)
-    status = models.CharField(max_length=9, choices=StatusEnum.choices, editable=False)
+    created_at = models.DateTimeField(auto_now_add=False)
+    payment = models.CharField('payment', max_length=127)
+    income = models.FloatField(blank=False)
+    status = models.CharField(max_length=9, choices=StatusEnum.choices)
     tariff = models.ForeignKey("Tariff", on_delete=models.CASCADE)
     userstatus = models.ForeignKey('UserStatus', on_delete=models.CASCADE)
 
